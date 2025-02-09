2089a000f23d4b95d0667bd8a7605cb02744b75e
Cybernetyc <Necromounger1990@gmail.com>
2025-02-09 10:28:15 +0300

    Small Refactor File main.c and temp_api_.c

--- DZ13_Course_Project/main.c
+++ DZ13_Course_Project/main.c
@@ -6,35 +6,35 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "struct.h"                 ///< Пользовательские заголовки
-#include "temp_api.h"                
-#include "module_for_FILE_read.h"   
+#include "temp_api.h"
+#include "module_for_FILE_read.h"
 
 int main (int argc, char *argv[])
-{ 
+{
   result_of_arg_t abc = interface(argc, argv);  ///< Обработка входных аргументов функции main
 
   if (abc.status == CLC) ///< Если ключ на подсчёт - обрабатываем файлы
-  {  
+  {
     int size_for_dynamic = FILE_Count_rec (abc.path);                ///< Грубо вычислили требуемое кол-во динамической памяти.
     sensor_t * info = malloc(size_for_dynamic * sizeof(sensor_t));   ///< Выделили память.
     int record_count = FILE_Read(abc.path, info);                    ///< Подсчитали кол-во записей.
-    
+
     /**
      * Если получен ключи для вычисления месячные температур
      * и номер месяца валиден
      */
     if ( abc.mounth >=1 && abc.mounth <= 12 )
     {
-      /** 
+      /**
        * Производим подсчёт:
        * 1. Средней температуры по заданному месяцу
        * 2. Минимальной температуры по заданному месяцу
        * 3. Максимальной температуры по заданному месяца
-       */  
+       */
       int AToM  = avr_temp_month(info, record_count, abc.mounth);
       int MiToM = min_temp_month(info, record_count, abc.mounth);
       int MaToM = max_temp_month(info, record_count, abc.mounth);
-      
+
       /**
        * Вывод вычисленных значений в stdout:
        * 1. Средней температуры по заданному месяцу
@@ -45,18 +45,18 @@ int main (int argc, char *argv[])
       printf("Minimum Temperature of %d month = %d\n", abc.mounth, MiToM);
       printf("Maximum Temperature of %d month = %d\n", abc.mounth, MaToM);
     }
-      else 
+      else
     {
-      /** 
+      /**
        * Производим подсчёт:
        * 1. Среднегодовой температуры
        * 2. Минимальной годовой температуры
        * 3. Максимальной годовой температуры
-       */  
+       */
       int AToY = avr_temp_year(info, record_count);
       int MiToY = min_temp_year(info, record_count);
       int MaToY = max_temp_year(info, record_count);
-      
+
       /**
        * Вывод вычисленных значений в stdout:
        * 1. Среднегодовой температуры
@@ -65,7 +65,7 @@ int main (int argc, char *argv[])
        */
       printf("Average Temperature of year = %d \n", AToY);
       printf("Minimal Temperature of year = %d \n", MiToY);
-      printf("Maximal Temperature of year = %d \n", MaToY);        
+      printf("Maximal Temperature of year = %d \n", MaToY);
     }
 
     free(info);   ///< Высвободили динамическую память
